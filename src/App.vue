1a7147bcc198b90ce00b0f8cfd1b3de4c7998268
VincenzoDePetris <vince02@live.it>
2023-09-08 15:52:42 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import appHeader from "./components/appHeader.vue";
+import appMain from "./components/appMain.vue";
+import appFooter from "./components/appFooter.vue";
+
+export default {
+  components: { appHeader },
+  components: { appMain },
+  components: { appFooter },
+};
+</script>
+
+<template>
+  <appHeader />
+  <appMain />
+  <appFooter />
+</template>
+
+<style lang="scss">
+@use "./components/general.scss" as *;
+</style>
